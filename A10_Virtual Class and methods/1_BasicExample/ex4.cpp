6f2b6cf2145e85f4772b220a428c2db48719880d
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-10-23 11:35:36 +0530

    Update ex4.cpp

--- A10_Virtual Class and methods/1_BasicExample/ex4.cpp
+++ A10_Virtual Class and methods/1_BasicExample/ex4.cpp
@@ -1,1 +1,36 @@
-
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void fun()
+    {
+        cout << "function in class A" << endl;
+    }
+};
+class B
+{
+public:
+    void fun()
+    {
+        cout << "function in class B" << endl;
+    }
+};
+class C : public A, public B
+{
+public:
+    void fun()
+    {
+        fun();
+        cout << "function in class C" << endl;
+    }
+};
+int main()
+{
+    C c;
+    c.fun(); // function in class c will be called
+    return 0;
+}
+// This will be an infinite recursion.
+// fun() in class C will call fun() in class C bez of the function overriding.
+// So no output will be printed and it will be an infinite recursion.
