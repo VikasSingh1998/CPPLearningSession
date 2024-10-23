d03678b8d050f3e5c5e73826b82073dddfed0cea
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-10-23 11:34:24 +0530

    Update ex3.cpp

--- A10_Virtual Class and methods/1_BasicExample/ex3.cpp
+++ A10_Virtual Class and methods/1_BasicExample/ex3.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void fun()
+    {
+        cout << "fun in class A" << endl;
+    }
+};
+class B
+{
+public:
+    void fun()
+    {
+        cout << "fun in class B" << endl;
+    }
+};
+class C : public A, public B
+{
+public:
+    void fun1()
+    {
+        cout << "fun1 in class C" << endl;
+        // fun();//Error
+        // error: reference to ‘fun’ is ambiguous --> which fun() to call
+        // to resolve the ambiuity problem, we have to use :: with class name
+        A::fun(); // fun in class A
+        B::fun(); // fun in class B
+    }
+};
 
+int main()
+{
+    C objc;
+    objc.fun1(); // fun1 in class C
+
+    return 0;
+}
+// className::fun() ==> to call the fun() in class "className"
