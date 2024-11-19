8e971857cbfbe80fea6e6c77496550c01c7d2267
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-19 17:22:52 +0530

    Update ex2.cpp

--- A11_Pointers/2_SmartPointer/ex2.cpp
+++ A11_Pointers/2_SmartPointer/ex2.cpp
@@ -1,5 +1,12 @@
-Types of smart pointer
------------------------
-1. unique_ptr
-2. shared_ptr
-3. weak_ptr
+Types of Smart Pointers
+===========================
+C++ libraries provide implementations of smart pointers in the following types:
+    1. auto_ptr ==> not used(depricated).
+    2. unique_ptr
+    3. shared_ptr
+    4. weak_ptr
+
+1. auto_ptr
+The auto_ptr was a smart pointer that was available in older versions of C++ (C++98 and C++03) 
+but has been deprecated in modern C++ standards (C++11 and later). 
+It was replaced by std::unique_ptr, which provides better safety and functionality.
