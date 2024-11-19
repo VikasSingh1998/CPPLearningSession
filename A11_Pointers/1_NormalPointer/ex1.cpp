1be70a29d088ea0e9ef602341e62f74efd7dd470
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-19 14:24:56 +0530

    Update ex1.cpp

--- A11_Pointers/1_NormalPointer/ex1.cpp
+++ A11_Pointers/1_NormalPointer/ex1.cpp
@@ -1,1 +1,21 @@
+// A pointer in C++ is a variable that holds the memory address of another variable.
+// It allows indirect access to the value stored at that memory address.
+//=-------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number = 42;
+    int *ptr = &number; // Pointer to an integer
 
+    cout << "Value of number: " << number << endl;
+    cout << "Address of number: " << &number << endl;
+    cout << "Value via pointer: " << *ptr << endl; // Dereferencing the pointer
+
+    return 0;
+}
+/*
+Value of number: 42
+Address of number: 0x61ff08
+Value via pointer: 42
+*/
