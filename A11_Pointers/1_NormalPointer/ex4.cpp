acbbe1a38f8adb4ea0be73bd43ec9e7a239aced6
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-19 14:32:09 +0530

    Update ex4.cpp

--- A11_Pointers/1_NormalPointer/ex4.cpp
+++ A11_Pointers/1_NormalPointer/ex4.cpp
@@ -46,4 +46,25 @@ int main()
 
     return 0;
 }
+===========================================================================
+Problems with Normal Pointers
+=============================
+Some Issues with normal pointers in C++ are as follows:
+1. Memory Leaks: 
+    This occurs when memory is repeatedly allocated by a program but never freed. 
+    This leads to excessive memory consumption and eventually leads to a system crash. 
 
+2. Dangling Pointers: 
+    A dangling pointer is a pointer that occurs at the time when the object is de-allocated 
+    from memory without modifying the value of the pointer.
+
+3. Wild Pointers: 
+    Wild pointers are pointers that are declared and allocated memory but the pointer is never initialized 
+    to point to any valid object or address.
+
+4. Data Inconsistency: 
+    Data inconsistency occurs when some data is stored in memory but is not updated in a consistent manner.
+
+5. Buffer Overflow: 
+    When a pointer is used to write data to a memory address that is outside of the allocated memory block. 
+    This leads to the corruption of data which can be exploited by malicious attackers.
