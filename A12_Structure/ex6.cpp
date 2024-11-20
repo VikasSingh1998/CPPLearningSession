c5b8f2356c868a3659b5ed67266024f4c81a89d5
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-20 12:12:29 +0530

    Update ex6.cpp

--- A12_Structure/ex6.cpp
+++ A12_Structure/ex6.cpp
@@ -1,1 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Address
+{
+    string city;
+    string state;
+    int zip;
+};
+struct Employee
+{
+    string name;
+    int id;
+    Address address;
+    double sal;
+    string dept[5];
+
+    // function to display the employee details
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Id: " << id << endl;
+        cout << "Address  of the employee: " << endl;
+        cout << "city: " << address.city << endl;
+        cout << "State: " << address.state << endl;
+        cout << "Zip: " << address.zip << endl;
+        cout << "Salary: " << sal << endl;
+        cout << "dept are: ";
+        for (auto str : dept)
+        {
+            if (!str.empty())
+            {
+                cout << str << ",";
+            }
+        }
+        cout << endl;
+    }
+};
+int main()
+{
+    // Create an Employee instance and initialize data
+    Employee emp = {
+        "John Doe",
+        12345,
+        {"New York", "NY", 10001},
+        75000.0,
+        {"Engineering", "Research", "", "", ""}};
+
+    // Call the display function
+    emp.display();
+
+    return 0;
+}
