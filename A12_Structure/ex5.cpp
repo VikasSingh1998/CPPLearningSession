0ee3ff3a0274b1eee2503eb144ce8abc1f370085
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-20 12:06:52 +0530

    Update ex5.cpp

--- A12_Structure/ex5.cpp
+++ A12_Structure/ex5.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Rectangle
+{
+    int length;
+    int breadth;
+    Rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+    void Area()
+    {
+        cout << "Area of rectangle: " << (length * breadth) << endl;
+    }
+};
 
+int main()
+{
+    Rectangle rec = Rectangle{10, 5};
+    // The instance of the structure is known as "Structure variable".
+    // The instance of the class is known as "Object of the class".
+    rec.Area();
+    return 0;
+}
