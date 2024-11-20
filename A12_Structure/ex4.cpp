d9fd344caa174ad83ea38b453097796c05b62785
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-20 12:05:28 +0530

    Update ex4.cpp

--- A12_Structure/ex4.cpp
+++ A12_Structure/ex4.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Rectangle
+{
+    int length;
+    int breadth;
+};
 
+int main()
+{
+    // Rectangle rec;
+    // rec.length = 10;
+    // rec.breadth = 5;
+    Rectangle rec = {10, 5};
+    cout << "Area of rectangle: " << (rec.length * rec.breadth) << endl;
+    return 0;
+}
