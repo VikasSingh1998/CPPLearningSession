24c32f9517069bdb59aa4b7c89a9ea9676a1818c
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-20 12:14:16 +0530

    Update ex7.cpp

--- A12_Structure/ex7.cpp
+++ A12_Structure/ex7.cpp
@@ -1,1 +1,20 @@
+// Structure Pointer ==>
+// We can have pointer to the structure.
+// If we have pointer to the structure then member are accessed by using -> operator insted of . operator.
+//=======================================================
+#include <bits/stdc++.h>
+using namespace std;
+struct Data
+{
+    string name;
+    int age;
+};
+int main()
+{
+    Data d1 = {"vikas", 24};
+    cout << d1.name << "-" << d1.age << endl;
 
+    Data *p = &d1; // pointer p holds the address of d1
+    cout << p->name << "==" << p->age << endl;
+    return 0;
+}
