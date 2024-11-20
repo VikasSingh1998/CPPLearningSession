e9394945e22b0dbfe64e081d723a9f677fb720ab
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-20 12:04:53 +0530

    Update ex3.cpp

--- A12_Structure/ex3.cpp
+++ A12_Structure/ex3.cpp
@@ -1,1 +1,24 @@
+// Create an array of type struct "Data" and store value in it.
+#include <bits/stdc++.h>
+using namespace std;
+struct Data
+{
+    string name;
+    int age;
+};
+int main()
+{
+    Data arr[10]; // create the array of size 10 of "Data" type.
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i].name = ("vikas" + to_string(i));
+        arr[i].age = i + 21;
+    }
 
+    // now display the content of array.
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i].name << "-" << arr[i].age << endl;
+    }
+    return 0;
+}
