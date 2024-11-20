c907842ce47c90e87a62115cad1ca86367d80255
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-20 12:15:24 +0530

    Update ex8.cpp

--- A12_Structure/ex8.cpp
+++ A12_Structure/ex8.cpp
@@ -1,1 +1,27 @@
+//  More complex example of a structure in C++ that includes pointers, dynamic memory allocation, and functions:
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Project
+{
+    string title;
+    double budget;
+};
+
+struct Employee
+{
+    string name;
+    int id;
+    int projectCount;
+    Project *project;
+
+    Employee(string n, int i, int pc)
+    {
+        name = n;
+        id = i;
+        projectCount = pc;
+
+        // create the array of size pc
+    }
+};
 
