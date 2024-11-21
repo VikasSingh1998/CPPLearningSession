ee301429495f394d05133dfe98b5175e5b095583
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-21 17:43:31 +0530

    Update ex3.cpp

--- A14_Enum/1_Enum(Traditional)/ex3.cpp
+++ A14_Enum/1_Enum(Traditional)/ex3.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
+enum data1
+{
+    val0,
+    val1,
+    val2,
+    val3,
+    val4
+};
+enum data2
+{
+    val0, // Error --> redeclaration of val0
+    val1, // Error --> redeclaration of val1
+    val2, // Error --> redeclaration of val2
+};
+int main()
+{
+    cout << "hello" << endl;
+    cout << data1::val0 << endl;
+    return 0;
+}
+// This problem is not there in the enum class.
+// since each enumerator will be locally maintained, not at global enum level.
