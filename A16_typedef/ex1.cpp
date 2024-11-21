2cfb5500088f41c4221680d24a845ae8bfb1df08
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-21 18:09:45 +0530

    Update ex1.cpp

--- A16_typedef/ex1.cpp
+++ A16_typedef/ex1.cpp
@@ -1,1 +1,14 @@
+// typedef ==> used to create new names (aliases) for existing types.
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+typedef unsigned long ulong;
+
+int main()
+{
+    ulong a = 12345678912345;
+    cout << a << endl;
+    return 0;
+}
 
