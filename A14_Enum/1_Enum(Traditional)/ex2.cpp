9cb1993980b504abb5d3823c70e283852b1f62ea
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-21 17:42:07 +0530

    Update ex2.cpp

--- A14_Enum/1_Enum(Traditional)/ex2.cpp
+++ A14_Enum/1_Enum(Traditional)/ex2.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
+enum days
+{
+    monday, // default value is 0
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday
+};
+int main()
+{
+    cout << days::friday << endl;//4 
+    return 0;
+}
