f52cfb585f8517cacc3e9f70a7697e6b5948cf55
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-21 17:56:44 +0530

    Update ex3.cpp

--- A14_Enum/2_EnumClass/ex3.cpp
+++ A14_Enum/2_EnumClass/ex3.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
+enum class days
+{
+    monday,
+    tuesday,
+    wednesday,
+    thursday = 10,
+    friday,
+    saturday
+};
+enum class daysList2
+{
+    monday, // no conflict with the previous namespace
+    tuesday,
+    wed
+};
+int main()
+{
+    // cout << days::friday << endl; // Error
+    /*
+    Your code will not compile because the enum class values cannot be directly used in cout.
+    The reason is that enum class values are strongly typed and not implicitly convertible to integers,
+    so cout doesn't know how to print them.
+    To fix this, you need to explicitly cast the enum class value to its underlying type, which is usually int by default.
+    */
+
+    cout << static_cast<int>(days::monday) << endl;    // 0
+    cout << static_cast<int>(days::wednesday) << endl; // 2
+    cout << static_cast<int>(days::thursday) << endl;  // 10
+
+    return 0;
+}
