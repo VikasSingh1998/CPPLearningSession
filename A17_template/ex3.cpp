fb8c9ee429cce9ce3928073382f4a846da06b3e5
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-26 16:25:47 +0530

    Update ex3.cpp

--- A17_template/ex3.cpp
+++ A17_template/ex3.cpp
@@ -1,1 +1,18 @@
+//How to take the different types arguments.
+#include <iostream>
+using namespace std;
 
+template<typename T1, typename T2>
+auto add(T1 a, T2 b) //>=C++ 14
+{
+    return a+b;
+}
+
+int main()
+{
+    cout<<add(5,10)<<endl;//15
+    cout<<add(5.10,10.10)<<endl;//15.2
+    cout<<add(15.5,10.9)<<endl;//26.4 
+    cout<<add(15,10.9)<<endl;//25.9
+    return 0;
+}
