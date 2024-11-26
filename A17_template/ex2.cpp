44945fda36cf5fb051b7158443562b9e5b23e2e1
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-11-26 16:15:14 +0530

    Update ex2.cpp

--- A17_template/ex2.cpp
+++ A17_template/ex2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+template<typename T>
+T add(T a, T b)
+{
+    return a+b;
+}
+
+int main()
+{
+    cout<<add(5,10)<<endl;//15
+    cout<<add(5.10,10.10)<<endl;//15.2
+    cout<<add(15.5,10.9)<<endl;//26.4 
+    cout<<add(15,10.9)<<endl;//error: no matching function for call to ‘add(int, double)’
+    return 0;
+}
