c73f6b4f4cc015ae9d07dfb4305950068380573e
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-12-23 12:29:22 +0530

    Update ex2.cpp

--- A20_STL/1_containers/5_Priority_Queue/ex2.cpp
+++ A20_STL/1_containers/5_Priority_Queue/ex2.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//We are creating the custom comp funtion.
+bool comp(int a,int b)
+{
+    return a>b;// Min-heap: lower values have higher priority
+}
+
+// Function to display the elements of the priority queue
+void display(priority_queue<int,vector<int>,decltype(&comp)> pq)
+{
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    //Create the MIN-HEAP by using the custom comp funtion
+    priority_queue<int,vector<int>,decltype(&comp)> pq(comp);// Pass comp to the constructor
+    
+    //push 7 random element in the PQ.
+    for(int i=0;i<7;i++)
+    {
+        pq.push(rand()%100);
+    }
+    display(pq);
+
+    return 0;
+}
+// 15 35 77 83 86 86 93 
+// ==========================================================
 
