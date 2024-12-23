a42866737aad6b0a88eaa1d3e19758d9c5d324e6
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2024-12-23 12:02:38 +0530

    Update ex1.cpp

--- A20_STL/1_containers/5_Priority_Queue/ex1.cpp
+++ A20_STL/1_containers/5_Priority_Queue/ex1.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void display(priority_queue<int> pq)
+{
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    //By default this will be the MAX-HEAP
+    priority_queue<int> pq1;
+    for(int i=0;i<7;i++)
+    {
+        pq1.push(rand()%100);
+    }
+    display(pq1);
+
+    return 0;
+}
+//PQ ==> By default MAX-HEAP
+// 93 86 86 83 77 35 15 
+
 
