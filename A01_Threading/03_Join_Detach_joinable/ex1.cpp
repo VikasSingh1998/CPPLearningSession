4109dbabe43ccd51c6b451d6a0cd5829a0b28a17
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-02-10 21:34:48 +0530

    Update ex1.cpp

--- A01_Threading/03_Join_Detach_joinable/ex1.cpp
+++ A01_Threading/03_Join_Detach_joinable/ex1.cpp
@@ -1,14 +1,14 @@
 /*
-detach()
-==========
-1. detach() is used to detach the newly created thread from the parent thread.
-2. Before deatching the thread --> we should always check whether the thared is joinable  or not.
-   If it is joinable then only it can be detached --> else program will be terminated.
-   i.e double detach() will result into the program termination.
+t1.join()
+===============
+1. Suppose  main thread created the thread t1 and and t1 is doing some operations.
+2. If Main thread executes the lines --> t1.join() , then main thread wait until t1 completes its executions.
 
-3. If we have detached thread and main thread function is returning --> then the detached thread execution is suspended.
--------------------------
-we should always write detach() or join() on the thread object, otherwise during the thared object distructor, program will be terminated.
+t1.joinable()
+=================
+The t1.joinable() function checks whether a thread object t1 is associated with a
+thread of execution. If the thread is joinable, it returns true, otherwise, it returns false.
+A thread is joinable if it has been started and has not yet been joined or detached.
 */
 
 #include <iostream>
@@ -21,9 +21,9 @@ void fun(int x)
     for (int i = 1; i <= x; i++)
     {
         std::cout << "Child thread executed " << i << " times" << std::endl;
-        // Sleep for a short duration to simulate work being done by the child thread
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    // Simulate some work with a sleep
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
 int main()
@@ -31,29 +31,32 @@ int main()
     // Create a thread (t1) that runs the function 'fun' with an argument of 10
     std::thread t1(fun, 10);
 
-    // Detach the child thread from the parent thread
-    // This allows t1 to run independently from the main thread
-    t1.detach();
-
-    // Main thread loop: Perform some work in the main thread
+    // Main thread loop
     for (int i = 1; i <= 10; i++)
     {
         std::cout << "Main thread executed " << i << " times." << std::endl;
-        // Sleep for a short duration to simulate work being done by the main thread
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
-    // Check if t1 is joinable (it shouldn't be since it's detached)
-    // Joining a non-joinable thread will result in a runtime error, so always check
+    // Main thread waits for t1 to finish
+    t1.join();
+
+    // t1.join();//double join result into the program termination.
+    /*
+    terminate called after throwing an instance of 'std::system_error'
+    what():  Invalid argument
+    -----------------
+    TO solve this problem ==> we can check whether the thread is joinable or not.
+    */
+    // Check if the thread is joinable before attempting to join
     if (t1.joinable())
     {
         t1.join(); // Main thread waits for t1 to finish
     }
     else
     {
-        std::cout << "Thread t1 is not joinable (already detached or finished)" << std::endl;
+        std::cout << "thread t1 is not joinable " << std::endl;
     }
 
-    // Return 0 to indicate successful completion of the program
+    // Return 0 to indicate successful completion
     return 0;
 }
