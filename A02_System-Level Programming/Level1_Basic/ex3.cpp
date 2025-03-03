5433c6bb3da5659183b759e9fde9b88c9f3c2f16
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-03 18:29:14 +0530

    Update ex3.cpp

--- A02_System-Level Programming/Level1_Basic/ex3.cpp
+++ A02_System-Level Programming/Level1_Basic/ex3.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int fd[2];// File descriptors for pipe
+    
+    if(pipe(fd) == -1)
+    {
+        cerr<<"pipe cration fail!!"<<endl;
+        return 1;
+    }
+    cout<<"Pipe is created successfully!!!"<<endl;
+    
+    //now pipe is created.
+    pid_t pid = fork();
+    cout<<pid<<endl;
+    
+    if(pid == 0) //child process
+    {
+        
+    }
+    else //Parent process
+    {
+        
+    }
+    
+
+    return 0;
+}
+
 
