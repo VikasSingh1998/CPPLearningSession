96ed906294ead158cddf7e9e553366bf47dae7f7
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-04 10:00:28 +0530

    Update ex3.cpp

--- A02_System-Level Programming/Level1_Basic/ex3.cpp
+++ A02_System-Level Programming/Level1_Basic/ex3.cpp
@@ -18,17 +18,30 @@ int main()
     pid_t pid = fork();
     cout<<pid<<endl;
     
-    if(pid == 0) //child process
+    if(pid == 0) //Child Process (Writes Data)
     {
-        
+        close(fd[0]);//child only write the data, so close the read end.
+        const char* msg = "Hello Vikas, Good Morning!!";
+        write(fd[1],msg,strlen(msg)+1);
+        close(fd[1]);//After writing the data, close the write end.
     }
-    else //Parent process
+    else //Parent Process (Reads Data)
     {
-        
+        close(fd[1]);//Parent only read the data, so close the write end.
+        char msg[50];//crate one char array to store the data after reading.
+        read(fd[0],msg,sizeof(msg));// Read data from pipe
+        cout<<"Parent: "<<msg<<endl;
+        close(fd[0]);//After reading, close the read end.
     }
     
-
     return 0;
 }
-
+//==========================================================================
+🔹 Code Execution Flow
+Parent process creates a pipe.
+Parent process forks a child process.
+Child writes a message into the pipe.
+Parent reads the message from the pipe.
+Both processes close their respective pipe ends.
+----------------------------------------------------------------------------
 
