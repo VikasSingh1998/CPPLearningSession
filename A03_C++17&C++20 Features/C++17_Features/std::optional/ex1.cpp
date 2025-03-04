454fed6d9d0dbe742202c0cd8fd0c93e9f0d8f89
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-04 17:04:13 +0530

    Update ex1.cpp

--- A03_C++17&C++20 Features/C++17_Features/std::optional/ex1.cpp
+++ A03_C++17&C++20 Features/C++17_Features/std::optional/ex1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <optional>
+#include <string>
 
+using namespace std;
+
+// Function to find a username by ID
+optional<string> findUserByID(int id)
+{
+    if(id == 1) return "user1_vikas";
+    else if(id == 2) return "user2_ravi";
+    else if(id == 3) return "user3_prashant";
+    else return nullopt;
+}
+
+int main()
+{
+    optional<string> username = findUserByID(4);
+    
+    //Check if the value exists inn the username.
+    if(username.has_value())
+    {
+        cout<<"User Name is: "<<*username<<endl;
+    }
+    else
+    {
+        cout<<"No user found for the given ID."<<endl;
+    }
+    
+    return 0;
+}
