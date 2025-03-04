b0e5fd58b3ffc6b5cfda11c3a54350e7987252ac
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-04 11:51:04 +0530

    Update ex4.cpp

--- A02_System-Level Programming/01_Inter-Process Communication (IPC)/1_Pipes/ex4.cpp
+++ A02_System-Level Programming/01_Inter-Process Communication (IPC)/1_Pipes/ex4.cpp
@@ -1,1 +1,7 @@
+Theory+Code
+-----------
+The mkfifo command in Linux creates a FIFO (First-In-First-Out) special file that allows processes to communicate with each other. 
 
+What does mkfifo do? 
+==> Creates a FIFO file, Sets permissions for the FIFO file, and Creates intermediate directories if needed. 
+--------------------------------------------------------------------------------------------------------------------------------------
