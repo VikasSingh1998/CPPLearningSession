dbafe14f5726e20dc2a5d91b30460b2cc3be4b07
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-04 14:24:02 +0530

    Update ex5.cpp

--- A02_System-Level Programming/01_Inter-Process Communication (IPC)/1_Pipes/ex5.cpp
+++ A02_System-Level Programming/01_Inter-Process Communication (IPC)/1_Pipes/ex5.cpp
@@ -1,1 +1,12 @@
+🔹 Bi-Directional Communication Using Named Pipes (FIFO)
+In the previous example, communication was one-way (writer → reader). Now, we will implement two-way communication where two processes can both send and receive messages.
 
+1️⃣ How to Achieve Bi-Directional Communication?
+To enable both processes to send and receive data, we need two FIFOs:
+
+"fifo1" → Used by Process A to send data to Process B.
+"fifo2" → Used by Process B to send data to Process A.
+
+
+==================================================================
+Do later, not necessary for the interview.
