657aa0ec992988f5c384960bfc003e0da255838b
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-05 14:32:20 +0530

    Update ex2.cpp

--- A21_Function/1_LambdaFunction/1_Basic/ex2.cpp
+++ A21_Function/1_LambdaFunction/1_Basic/ex2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    auto fun = [](){
+        cout<<"this is the first lambda function!"<<endl;
+    };
+    fun();
+    return 0;
+}
+/*
+auto funName = []() -> RT {};
+
+if the RT is not there, no need to  the write ->
+*/ 
