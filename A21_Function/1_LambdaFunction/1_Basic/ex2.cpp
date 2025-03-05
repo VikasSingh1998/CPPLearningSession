2cb7b9f84a4f03ea49e36b003f2afd6f39870b36
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-05 14:43:27 +0530

    Update ex2.cpp

--- A21_Function/1_LambdaFunction/1_Basic/ex2.cpp
+++ A21_Function/1_LambdaFunction/1_Basic/ex2.cpp
@@ -1,1 +1,18 @@
+2. Lambda with Parameters and return type
+------------------------------------------
+#include <iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    auto add = [](int a,int b) -> int{
+        return a+b;
+    };
+    cout<<add(3,5)<<endl;
+    return 0;
+}
+--------------------------------------------
+Explanation:
+  auto add = ...; =====>  Stores the lambda in a variable.
