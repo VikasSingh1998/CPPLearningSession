f7fd49c232e87e373f78a717a92c5277e110cfe9
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-05 14:39:02 +0530

    Update ex2.cpp

--- A21_Function/1_LambdaFunction/1_Basic/ex2.cpp
+++ A21_Function/1_LambdaFunction/1_Basic/ex2.cpp
@@ -1,5 +1,18 @@
+Basic Syntax of Lambda Function
+----------------------------------
+auto functionName = [capture](parameters) -> return_type { 
+    // function body
+};
+
+Parts of a Lambda Function
+---------------------------
+Capture Clause [] → Captures variables from the surrounding scope.
+Parameter List () → Specifies input arguments (like a regular function).
+Return Type -> return_type (optional) → Specifies the return type.
+Function Body {} → Contains the actual logic of the lambda function.
+========================================================================
 1. Simplest Lambda Function (No Parameters, No Captures)
-//=========================================================
+---------------------------------------------------------
 #include <iostream>
 #include <bits/stdc++.h>
 
