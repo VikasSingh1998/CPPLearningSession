72f364c2dbe8f16ddacdcd1e0103c443967f2709
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-07 14:21:33 +0530

    Update ex4.cpp

--- A21_Function/1_LambdaFunction/1_Basic/ex4.cpp
+++ A21_Function/1_LambdaFunction/1_Basic/ex4.cpp
@@ -13,3 +13,50 @@ int main() {
 
 It allows storing lambdas and function pointers in a flexible way.
 -----------------------------------------------------------------------------
+6. Mutable Lambda (Modify Captured Variables)
+---------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+
+    auto increment = [count]() mutable -> void  
+    { 
+        count++; 
+        cout << "Count: " << count << endl; 
+    };
+    
+    increment();
+    increment();
+
+    cout << "Outside Lambda, Count: " << count << endl; // Remains 0
+    return 0;
+}
+/*
+Count: 1
+Count: 2
+Outside Lambda, Count: 0
+*/
+💡 Why mutable?
+Captured variables (by value) cannot be modified inside the lambda unless mutable is used.
+==================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
