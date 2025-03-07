6d2de03d05ce496070d4f2beee7c5125c2bd6a8b
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-07 14:12:06 +0530

    Update ex4.cpp

--- A21_Function/1_LambdaFunction/1_Basic/ex4.cpp
+++ A21_Function/1_LambdaFunction/1_Basic/ex4.cpp
@@ -1,1 +1,15 @@
+5. Using Lambda in std::function
+=-----------------------------------
+#include <iostream>
+#include <functional>  // Required for std::function
+using namespace std;
 
+int main() {
+    function<int(int, int)> multiply = [](int a, int b) { return a * b; };
+    cout << "Product: " << multiply(4, 5) << endl;
+    return 0;
+}
+💡 Why std::function?
+
+It allows storing lambdas and function pointers in a flexible way.
+-----------------------------------------------------------------------------
