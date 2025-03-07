5ad16fdf1a3b86535caf65cced8500c4cdcb1443
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-07 12:05:05 +0530

    Update ex1.cpp

--- A21_Function/2_std::function<>/ex1.cpp
+++ A21_Function/2_std::function<>/ex1.cpp
@@ -1,10 +1,10 @@
 1️⃣ What is std::function<>?
 🔹 std::function<> is a general-purpose function wrapper that can store:
-Lambda functions
-Function pointers
-Functor (Function objects)
-Member functions (with std::bind)
-Any callable entity with a matching signature
+==> Lambda functions
+==> Function pointers
+==> Functor (Function objects)
+==> Member functions (with std::bind)
+==> Any callable entity with a matching signature
 
 🔹 Basic Syntax:
 std::function<return_type(parameter_types)> func_name;
