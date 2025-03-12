94a116dcbad14a4726738bc5d7faef5dfa448d0b
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-12 19:28:09 +0530

    Update ex1.cpp

--- A01_Threading/02_Synchronization/03_std::lock_guard/ex1.cpp
+++ A01_Threading/02_Synchronization/03_std::lock_guard/ex1.cpp
@@ -1,1 +1,6 @@
-
+std::lock_guard ==> automatically locks a std::mutex when it is created and unlocks it when it goes out of scope.
+---------------------------------------------------------------------------------------------------------------------
+1. std::lock_guard is a RAII (Resource Acquisition Is Initialization)-based locking mechanism that 
+   automatically locks a std::mutex when it is created and unlocks it when it goes out of scope. 
+2. This eliminates the need to manually call lock() and unlock(), preventing accidental deadlocks.
+---------------------------------------------------------------------------------------------------------------------
