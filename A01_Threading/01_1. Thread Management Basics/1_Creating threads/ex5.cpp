42fd1988fd1bc92297f97b4e7d904ee1fb26286c
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-12 15:08:11 +0530

    Update ex5.cpp

--- A01_Threading/01_1. Thread Management Basics/1_Creating threads/ex5.cpp
+++ A01_Threading/01_1. Thread Management Basics/1_Creating threads/ex5.cpp
@@ -1,1 +1,46 @@
+// Thread creation by using the  static member funtion
+//=====================================================
+#include <iostream>
+#include <thread>
 
+class MyClass
+{
+public:
+    // fun is the  static member function.
+    static void fun(int x)
+    {
+        int i = 1;
+        while (i <= x)
+        {
+            std::cout << "Child thread executed " << i << " times" << std::endl;
+            i++;
+        }
+    }
+};
+
+int main()
+{
+    // create the instance of MyClass
+    MyClass obj;
+
+    // Create a thread (t1) that runs the static member function with an argument of 10
+    std::thread t1(&MyClass::fun, 10);
+
+    // This loop will be executed by the main thread
+    for (int i = 1; i <= 10; i++)
+    {
+        std::cout << "Main thread executed " << i << " times." << std::endl;
+    }
+
+    // Main thread waits for t1 to finish
+    t1.join();
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
+/*
+Creating a thread that runs a static member function in C++ is straightforward
+because static member functions do not operate on specific instances of a class
+and do not require an object instance to be called.
+--------------------------------------------------------------------------------------
+*/
