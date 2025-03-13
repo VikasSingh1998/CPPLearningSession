98fa4665d8e0fc9f750bc2054a9baecb41b725a5
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-03-13 10:06:58 +0530

    Update ex3.cpp

--- A01_Threading/02_Synchronization/04_std::unique_lock/ex3.cpp
+++ A01_Threading/02_Synchronization/04_std::unique_lock/ex3.cpp
@@ -1,1 +1,7 @@
-
+Deferred Locking with std::unique_lock
+We can delay locking using std::defer_lock and manually lock later.
+----------------------------------------------------------------------------
+Syntx:
+======
+std::unique_lock<std::mutex> lock(mtx, std::defer_lock); // Does NOT lock immediately
+lock.lock(); // Locking when needed
