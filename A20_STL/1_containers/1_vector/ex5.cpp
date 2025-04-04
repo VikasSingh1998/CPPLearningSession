60e6310357cca42b3f9fc65bea84a57852770893
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:50:06 +0530

    Update ex5.cpp

--- A20_STL/1_containers/1_vector/ex5.cpp
+++ A20_STL/1_containers/1_vector/ex5.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dispVector(const vector<int> &v)
+{
+    // v.push_back(4); ==> if we will not give the const --> we can change the v content inside this function.
+    // so whenever we want that that some value shuld not be change inside some method --> we should declare it as const.
+    for (auto e : v)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    // Creating a vector of integers
+    vector<int> v(10);
+    cout << v.capacity() << endl; // 10
+    cout << v.size() << endl;     // 10
+
+    for (int i = 0; i < 10; i++)
+    {
+        v.push_back(i + 1);
+    }
+    cout << v.capacity() << endl; // 20
+    cout << v.size() << endl;     // 20
+
+    dispVector(v);
+    return 0;
+}
 
