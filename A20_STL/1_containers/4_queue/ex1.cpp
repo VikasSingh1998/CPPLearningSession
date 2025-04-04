39432c239bf42dc674cda357992640cb17a4a7a0
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:55:07 +0530

    Update ex1.cpp

--- A20_STL/1_containers/4_queue/ex1.cpp
+++ A20_STL/1_containers/4_queue/ex1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // queue<int> q1;//create an empty queue q1
+    // queue<int> q2(q1);//create Q q2 by copying elements of q1
+    //-------------------------
+    // q.empty() ==> return true if q is empty
+    // q.push(value) ==> push the value into the queue.
+    // q.pop() ==> remove the front element of the queue
+    // q.front() ==> return the front element of the queue but not remove.
+
+    return 0;
+}
 
