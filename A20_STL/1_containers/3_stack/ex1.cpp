1c8e0318bf50a6ad69a9f6fcd62576cacacade76
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:53:53 +0530

    Update ex1.cpp

--- A20_STL/1_containers/3_stack/ex1.cpp
+++ A20_STL/1_containers/3_stack/ex1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // stack ==> it is a container adapter
+    // we can't create iterator on stack.-> we can see only top elements of the stack.
+    //-------------------------
+    //  empty() ==> return stack is empty or not.
+    //  while(!s.empty()){do something}
+    //-------------------------
+    //  void push(value) ==> push the value on the top of the stack
+    //  void pop() ==> only remove the top element, not return it.
+    //  int top() ==> return the top element of the stack, not remove it.
+    //  int size() ==> return the size of the the stack.
+    //  s1.swap(s2) ==> swap the 2 stack, internally onlu name will be swapped.
+    //
+    return 0;
+}
 
