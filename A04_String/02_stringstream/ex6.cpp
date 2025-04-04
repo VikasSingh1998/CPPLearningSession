6b66fe097650319107009d9fc8db9eb155db943b
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:27:37 +0530

    Update ex6.cpp

--- A04_String/02_stringstream/ex6.cpp
+++ A04_String/02_stringstream/ex6.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+// To use :: as a separator instead of the default newline character,
+// you can use the overloaded version of getline which allows you to
+// specify a custom delimiter.
+#include <sstream>
+using namespace std;
+int main()
+{
+    string str = "CSE::CPP::121";
+
+    // create the object of stringstream.
+    stringstream sso(str);
+
+    string line;
+    while (getline(sso, line, ':'))
+    {
+        // Since we are splitting by "::",
+        // we need to ignore the extra ':'
+        if (sso.peek() == ':')
+        {
+            sso.ignore();
+        }
+        cout << line << endl;
+    }
+
+    return 0;
+}
+// CSE
+// CPP
+// 121
+//==========================================----------------
+// We use getline with a third parameter ':' to specify the delimiter.
+// We then use sso.peek() to check if the next character is also a colon (:).
+// If it is, we call sso.ignore() to skip the extra colon.
 
