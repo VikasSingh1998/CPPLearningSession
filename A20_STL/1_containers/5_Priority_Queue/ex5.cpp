42616539b8bd93a7a02e483707879c31b86d5aea
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:58:00 +0530

    Update ex5.cpp

--- A20_STL/1_containers/5_Priority_Queue/ex5.cpp
+++ A20_STL/1_containers/5_Priority_Queue/ex5.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <bits/stdc++.h>
+using namespace std;
+//-------------------------
+void displayPQ(priority_queue<int> pq)
+{
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+}
+//-------------------------
+int main()
+{
+    priority_queue<int> pq; // maxHeap
+    srand(time(0));
+    for (int i = 0; i < 5; i++)
+    {
+        pq.push((rand() % 100) + 1); // generate random number between 0 to 99 and add 1 to make [0,100]
+    }
+    displayPQ(pq);
+    return 0;
+}
 
