903c932ad5cbc6405cf2b360d20c1581f4c394f2
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 12:01:32 +0530

    Update ex3.cpp

--- A20_STL/1_containers/6_deque/ex3.cpp
+++ A20_STL/1_containers/6_deque/ex3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------
+void displayDQ(deque<int> dq)
+{
+    for (auto it = dq.begin(); it != dq.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+//----------------------------
+int main()
+{
+    deque<int> dq(5, 0); // create the deque of size 5 with all the value set to 0
+    displayDQ(dq);       // 0 0 0 0 0
+    //----------------------------
+    dq.assign({1, 2, 3, 4, 5});
+    displayDQ(dq); // 1 2 3 4 5
+    //----------------------------
+    dq.insert(dq.begin(), {10, 20, 30});
+    displayDQ(dq); // 10 20 30 1 2 3 4 5
+    //----------------------------
+    dq.erase(dq.begin(), dq.begin() + 3); // remove the first dq,dq+1,dq+2 --> first 3 element from the deque
+    displayDQ(dq);                        // 1 2 3 4 5
+    //----------------------------
+
+    return 0;
+}
 
