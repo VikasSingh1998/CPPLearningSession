0fd30a81f59b1d52df469139626b23218e0a328c
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:43:11 +0530

    Update ex5.cpp

--- A10_Virtual Class and methods/4_VirtualBaseClass/ex5.cpp
+++ A10_Virtual Class and methods/4_VirtualBaseClass/ex5.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
+//--------------------------
+class A
+{
+public:
+    virtual void fun1()
+    {
+        cout << "fun1 in the class A" << endl;
+    }
+};
+
+class B : public A
+{
+public:
+    virtual void fun1()
+    {
+        cout << "fun1 in the class B" << endl;
+    }
+};
+
+class C : public A
+{
+public:
+    void fun1()
+    {
+        cout << "fun1 in the class C" << endl;
+    }
+};
+
+int main()
+{
+    A *ptra;
+    B objb;
+    C objc;
+
+    ptra = &objb;
+    ptra->fun1(); // fun1 in the class B
+
+    ptra = &objc;
+    ptra->fun1(); // fun1 in the class C
+    return 0;
+}
 
