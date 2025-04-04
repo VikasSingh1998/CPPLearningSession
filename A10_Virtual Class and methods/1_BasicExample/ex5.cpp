ed137d0c5a2e277a50956ef60d7d51316429c91b
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:29:43 +0530

    Update ex5.cpp

--- A10_Virtual Class and methods/1_BasicExample/ex5.cpp
+++ A10_Virtual Class and methods/1_BasicExample/ex5.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void fun()
+    {
+        cout << "function in class A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void fun()
+    {
+        cout << "function in class B" << endl;
+    }
+};
+
+int main()
+{
+    A obja = A();
+    obja.fun(); // function in class A
+
+    B objb = B();
+    objb.fun(); // function in class B
+
+    // A reference or pointer of the parent class can hold the child class object.
+    A obja1 = B();
+    obja1.fun(); // function in class A
+}
+/*
+what is the diff between
+A obja = A();
+and
+A* obja = new A();
+==============================================================
+A obja = A();
+-------------
+
+
+A* obja = new A(); ==> new A() returns the address (a pointer) to the dynamically allocated object of type A.
+-------------------------------------------------------------------------------------------------------------
+new A() ==> creates an object of type A on the heap.
+The memory address of this newly created object is returned.
+The pointer obja holds the address of the object.
+The object created by new A() will persist until you manually free the memory using delete ==>
+delete obja; // Frees the memory allocated for the object
+
+If you don't call delete, the memory will not be reclaimed, causing a memory leak.
+
+
+*/
 
