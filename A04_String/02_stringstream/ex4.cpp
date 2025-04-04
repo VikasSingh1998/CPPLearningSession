66c3817ee8ef2368d5c107deb2c94c79edeedf56
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:26:55 +0530

    Update ex4.cpp

--- A04_String/02_stringstream/ex4.cpp
+++ A04_String/02_stringstream/ex4.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    string str = "CSE CPP 121"; // Initialize a string with the value "CSE CPP 121"
+
+    // Create an object of stringstream and initialize it with the input string
+    stringstream sso(str);
+
+    string line1;
+    // Use getline to read the entire content of the stringstream into line1
+    getline(sso, line1);
+
+    // Output the content of line1
+    cout << line1 << endl; // This will print: CSE CPP 121
+
+    return 0;
+}
 
