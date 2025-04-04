26f71c58720827246d4445155969cb051ccde546
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:42:32 +0530

    Update ex3.cpp

--- A10_Virtual Class and methods/4_VirtualBaseClass/ex3.cpp
+++ A10_Virtual Class and methods/4_VirtualBaseClass/ex3.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+//--------------------------
+class A
+{
+    virtual void fun1()
+    {
+        cout << "fun1 in the class A" << endl;
+    }
+};
 
+class B : public A
+{
+};
+
+class C : public A
+{
+};
+
+class D : public B, public C
+{
+public:
+    void fun2()
+    {
+        // fun1();
+
+        cout << "fun2 in the class D" << endl;
+    }
+};
+
+int main()
+{
+    D objd;      // Creating an object of class D
+    objd.fun2(); // Calls fun2() in class D
+    return 0;
+}
+/*
+Here there are 2 fun() function in class D,but jab tak usko call nhi karenge tab tk koi bhi error nahi aayega,
+Compiler will give an error only when we call the fun1().
+ */
