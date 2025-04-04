160f33f424171fc3e03a84e380db341bd5db8a43
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:58:20 +0530

    Update ex6.cpp

--- A20_STL/1_containers/5_Priority_Queue/ex6.cpp
+++ A20_STL/1_containers/5_Priority_Queue/ex6.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <bits/stdc++.h>
+using namespace std;
+//-------------------------
+void displayPQ(priority_queue<int, vector<int>, greater<int>> pq)
+{
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+}
+//-------------------------
+int main()
+{
+    priority_queue<int, vector<int>, greater<int>> pq; // minHeap
+    // we have to give 3 things.
+    // datatype==> which datatype  we have to store.
+    // container ==> where we will store this datatype
+    // comparator class ==> class where we will compare the object.
+    // default is,greater is template so, greater<int> is class.
+
+    srand(time(0));
+    for (int i = 0; i < 5; i++)
+    {
+        pq.push((rand() % 100) + 1); // generate random number between 0 to 99 and add 1 to make [0,100]
+    }
+    displayPQ(pq);
+    return 0;
+}
 
