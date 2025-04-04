e83f2dcbb3482a53516cc740fe5c463d6959aeac
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-04-04 11:56:29 +0530

    Update ex2.cpp

--- A20_STL/1_containers/4_queue/ex2.cpp
+++ A20_STL/1_containers/4_queue/ex2.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//-------------------------
+template <class T>
+void printQueue(queue<T> q)
+{
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+//-------------------------
+int main()
+{
+    queue<int> q;
+    for (int i = 0; i < 5; i++)
+    {
+        q.push(i + 1);
+    }
+    cout << q.size() << endl; // 5
+    printQueue(q);            // 1 2 3 4 5
+
+    return 0;
+}
 
