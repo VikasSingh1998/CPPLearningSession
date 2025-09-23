adbf8c475a607e04e04fb42beb1b8b0357102912
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-09-24 02:08:15 +0530

    Update ex1.cpp

--- A01_Threading/02_Synchronization/03_std::lock_guard/ex1.cpp
+++ A01_Threading/02_Synchronization/03_std::lock_guard/ex1.cpp
@@ -4,3 +4,14 @@ std::lock_guard ==> automatically locks a std::mutex when it is created and unlo
    automatically locks a std::mutex when it is created and unlocks it when it goes out of scope. 
 2. This eliminates the need to manually call lock() and unlock(), preventing accidental deadlocks.
 ---------------------------------------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------------------------------------------------------
+Mutex ko manually lock() aur unlock() karna thoda risky hota hai, 
+kyunki agar programmer bhool gaya unlock karna (ya exception aagayi), to deadlock ho sakta hai.
+
+Is problem ko avoid karne ke liye C++ STL ne std::lock_guard diya hai.
+🔹 What is lock_guard?
+👉 A lock_guard is a wrapper around mutex which automatically acquires the lock when created and releases it when destroyed (RAII principle).
+
+Matlab tumhe khud unlock() karne ki zarurat nahi hai.
+----------------------------------------------------------------------------------------------------------------------
