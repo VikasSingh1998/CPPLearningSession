8015f4cb916c831697e491ed3f37bbef6583166f
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-09-24 01:37:42 +0530

    Update ex2.cpp

--- A01_Threading/02_Synchronization/01_Mutex/ex2.cpp
+++ A01_Threading/02_Synchronization/01_Mutex/ex2.cpp
@@ -1,1 +1,34 @@
+Race Condition Example (without mutex)
+--------------------------------------
+#include <iostream>
+#include <thread>
 
+int counter = 0;  // shared resource
+
+void increment() {
+    for(int i = 0; i < 100000; i++) 
+    {
+        //no synchronization
+        counter++;  // multiple threads update at same time ❌
+        //read-modify-write is not atomic operation
+    }
+}
+
+int main() {
+    std::thread t1(increment);
+    std::thread t2(increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Final Counter: " << counter << "\n";
+    return 0;
+}
+---------------------------------------
+📝 Expected Output:
+Ideally, counter = 200000 (100000 from each thread).
+
+⚠️ But Actual Output:
+Kabhi 150000, kabhi 170000, kabhi 180000... different every run.
+
+Reason → Race Condition (dono threads ek hi time pe counter ko update kar rahe hain).
