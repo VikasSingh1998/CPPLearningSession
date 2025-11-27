b42602f204595f26830bb65bb9a7495588481791
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-11-27 23:57:14 +0530

    Update ex1.cpp

--- A01_Threading/01_1. Thread Management Basics/6. this_thread/ex1.cpp
+++ A01_Threading/01_1. Thread Management Basics/6. this_thread/ex1.cpp
@@ -1,1 +1,21 @@
+Examples
+-----------
+std::this_thread::get_id() ==> Get current thread's ID
+
+std::this_thread::sleep_for()  ==> Pause thread for a duration
+
+std::this_thread::sleep_until()  ==> Pause until a specific time
+
+std::this_thread::yield()  ==> Yield execution to allow other threads to run
+--------------------------------------------------------------------------------
+Example:
+std::cout << "Main thread ID: " << std::this_thread::get_id() << std::endl;
+-------------------------------------------------------------------------------
+std::this_thread::sleep_for(std::chrono::seconds(2));
+-------------------------------------------------------------------------------
+std::this_thread::yield(); // Give chance to other threads
+-------------------------------------------------------------------------------
+
+
+
 
